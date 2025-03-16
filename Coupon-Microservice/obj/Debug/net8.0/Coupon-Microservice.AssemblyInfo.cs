38e3a700a3fdb7ac1452dfa5013b1f34bc67f6a5
sahildhola7 <sahildhola2305@gmail.com>
2025-03-16 14:41:10 +0200

    v-2

--- Coupon-Microservice/obj/Debug/net8.0/Coupon-Microservice.AssemblyInfo.cs
+++ Coupon-Microservice/obj/Debug/net8.0/Coupon-Microservice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Coupon-Microservice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bf60c763e63c2e7fdf05544f0f9e8a506457081")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb337e6099194260085ee0fd284ad4e48560729f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Coupon-Microservice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Coupon-Microservice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
